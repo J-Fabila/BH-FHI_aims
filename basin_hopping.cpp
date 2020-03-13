f4f8fa3ca934337932bd2f25411a040d10411109
Jonathan Luque <56845748+jluque21@users.noreply.github.com>
2020-03-13 09:58:54 -0600

    Update basin_hopping.cpp

--- basin_hopping.cpp
+++ basin_hopping.cpp
@@ -1,3 +1,6 @@
+#include<stdio.h>
+#include<stdio.h>
+#include<unistd.h>
 #include"atomicpp.h"
 string Simbolo_1, Simbolo_2, file_name, command, aux,geometry_file, initialization_file, outputfile, i_str, E_str, tag;
 int continue_alg,  Ncore, randomness, kick, iteraciones,swap_step, contenido, m, N_Simbolo_1, N_Simbolo_2, count, fail_counter=0, resto, failed_max,crystal;
@@ -40,30 +43,32 @@ if(crystal==1)  //Esto sustituye tener que poner [x_min,x_max]; [y_min,y_max]...
 }
 int i = 1;
 
-if(continue_alg==1){
+if(continue_alg==1)
+{
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   //                                      RESTART ALGORITHM                                         //
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
-      string iteration_counter_i ="cd ";
-             iteration_counter_i+=file_name;
-             iteration_counter_i+=" ; for i in $(ls coord*xyz ); do head -2 $i | tail -1 | awk '{ print $2 }' ; done | sort -n  | tail -1";
-      i=int_pipe(iteration_counter_i,1);
+      chdir(file_name.c_str());
+      string iteration_counter_i ="ls coord*xyz | wc -l";
+      i=int_pipe(iteration_counter_i);
 
-      string iteration_counter_m ="cd ";
-             iteration_counter_m+=file_name;
-             iteration_counter_m+="/rejected ; ls coord*xyz 2> /dev/null | wc -l ";
-      m=int_pipe(iteration_counter_m,0);
+      string iteration_counter_m ="ls ./rejected/coord*xyz | wc -l ";
+      m=int_pipe(iteration_counter_m);
 }
-else{
+
+
+else
+{
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   //                                        BEGIN ALGORITHM                                         //
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
    // Creates work directory
-   command ="if [ -d "+file_name+" ] ; then mv "+file_name+" other_"+file_name;
+   command ="if [ -d "+file_name+" ] ; then mv "+file_name+" other_"+file_name; ///// Modify to be in c++ code??
    command+=" ; fi ; mkdir "+file_name+" ; cd "+file_name+"  ; mkdir rejected ;";
    command+=" cp ../input/* .";
    system(command.c_str());
    command.clear();
+   chdir(file_name.c_str());
    i=1; m=0;
    contenido=0;
    count=1;
@@ -74,7 +79,7 @@ else{
       //Inicializa archivo geometry.in
         command ="cp ";
         command+=initialization_file;
-        command+=" "+file_name+"/geometry.in";
+        command+="geometry.in";
         system(command.c_str());
         command.clear();
         count++;
@@ -107,159 +112,246 @@ else{
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
          if(crystal==0){
             geometry_file.clear();
-            geometry_file=file_name+"/geometry.in";
+            geometry_file="geometry.in";
             clus.print_fhi(geometry_file);
          }
          else{
             clus.move((x_max-x_min)/2.0+random_number(-dist,dist),(y_max-y_min)/2.0+random_number(-dist,dist),z_max-clus.z_min());
             geometry_file.clear();
-            geometry_file=file_name+"/geometry.tmp";
+            geometry_file="geometry.tmp";
             clus.print_fhi(geometry_file);
             command.clear();
-            command="cd "+file_name+" ; cat crystal.in > geometry.in ; cat geometry.tmp >> geometry.in ; rm geometry.tmp ";
+            command="cat crystal.in > geometry.in ; cat geometry.tmp >> geometry.in ; rm geometry.tmp ";
             system(command.c_str());
             command.clear();
          }
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
       }
-      command="cd "+file_name+" ; ./run.sh";
+
+      // RUN FIRST CONFIGURATION:
+      command="./run.sh";
       system(command.c_str());
       command.clear();
-      command="cd "+file_name+" ; grep 'Have a nice day' output.out | wc -l";
+      command="grep 'Have a nice day' output.out | wc -l";
       contenido=int_pipe(command.c_str());
       command.clear();
    }
-   command="cd "+file_name+" ; grep \" | Total energy of the DFT \" output.out | awk '{print $12}' ";
+
+
+   /// Store energy and optimized geometry:
+   command="grep \" | Total energy of the DFT \" output.out | awk '{print $12}' ";
    Energy=double_pipe(command.c_str());
    i_str=to_string(i);
-   E_str=string_pipe(command); //Better for Eneregies with all the value
+   E_str=string_pipe(command); //Better for Energies with all the value
    command.clear();
-   command=file_name+"/geometry.in.next_step";
-  
-  
-  //cout<<command<<endl;
-  //////////////////////////////////////
-   clus.read_fhi(command);      //////// Puede no estar leyendo el archivo correcto (geometry.in.next_step)
-  //////////////////////////////////////
-  
+
+   // get initial, relaxed geometry and store it as xyz (first xyz!)
+   command="geometry.in.next_step";
+   clus.read_fhi(command);
    command.clear();
-   command=file_name+"/coordinates1.xyz";
+   command="coordinates1.xyz";
    tag.clear();
    tag=" Iteration "+i_str+" -----> Energy = "+E_str+" eV ";
    clus.print_xyz(command,tag);
    command.clear();
-   command="cd "+file_name+" ; mv output.out output1.out ; ";
-   command+=" mv geometry.in geometry1.in ; echo 'Step ----> Energy[eV]' >> energies.txt ; echo '1 ---->' "+E_str+" >> energies.txt";
+
+
+   // store first output and geometry files:
+   command="mv output.out output1.out";
    system(command.c_str());
    command.clear();
 
-   cout<<" --> Relaxation of initial configuration: DONE! "<<endl<<endl;
-   cout<<"=================================================================="<<endl;
+   command="mv geometry.in geometry1.in";
+   system(command.c_str());
+   command.clear();
+
+   command="echo 'Step ----> Energy[eV]' >> energies.txt ; echo '1 ---->' "+E_str+" >> energies.txt";
+   system(command.c_str());
+   command.clear();
+
+
+
+   // NOW THE BASIN HOPPING WILL BEGIN:
+
+   cout<<" --> Relaxation of initial configuration: DONE! "<<endl;
+   cout<<"================================================================================================"<<endl;
    cout<<"BH-DFT routine starts here! "<<endl;
    cout<<"Note: "<<endl;
    cout<<"For monometallic clusters: only random xyz moves will be applied "<<endl;
    cout<<"For bimetallic clusters  : 1 atomic swap will be performed after "<<swap_step<<" moves "<<endl;
-   cout<<"==================================================================="<<endl<<endl;
+   cout<<"================================================================================================"<<endl;
+
+
+   // set up BH-DFT iteration counter to 2:
    i=2;
+
+
+// finished else for initial, randomly generated structure!
 }
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// BASIN HOPPING LOOP:
+// get coordinates and apply kick
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 while(i+m <= iteraciones)
 {
+  // when to do atomic swap:
   resto=i%swap_step;
-  geometry_file.clear();
-  geometry_file=file_name+"/geometry.in.next_step";
-  command.clear(); command="cat "+geometry_file;
-  system(command.c_str());
-  command.clear();
+
+  // cleaning memory
   i_str.clear();
   E_str.clear();
   i_str=to_string(i);
-  // Get cluster coordinates from output file
-  if(N_Simbolo_2>0) //es bimetalico
-  {    
-    clus_1=extract(geometry_file,Simbolo_1);
-    clus_2=extract(geometry_file,Simbolo_2);
-    clus  =clus_1+clus_2;
-  }
-  else //es monometalico
+
+
+  // For bimetallics:
+  if(N_Simbolo_2>0)
   {
-    clus=extract(geometry_file,Simbolo_1);
+   // get relaxed coordinates from initial configuration (i=1):
+   geometry_file.clear();
+   geometry_file="geometry.in.next_step";
+   command.clear();
+   command="cat "+geometry_file;
+   system(command.c_str());
+   clus_1=extract(geometry_file,Simbolo_1);
+   clus_2=extract(geometry_file,Simbolo_2);
+
+   clus  =clus_1+clus_2;
   }
-  // Applies swap or kick
+
+
+  // for monometallics:
+  else
+  {
+   // get relaxed coordinates from initial configuration (i=1):
+   geometry_file.clear();
+   geometry_file="geometry.in.next_step";
+   command.clear();
+   command="cat "+geometry_file;
+   system(command.c_str());
+   clus=extract(geometry_file,Simbolo_1);
+  }
+
+
+
+
+  /////////////////////////////////////
+  // MOVE CLASSES: KICK VS ATOMIC SWAP
+  /////////////////////////////////////
+
+
+  // Applies swap for bimetallic case by calling "atomic.cpp"
   if(resto==0 && N_Simbolo_2 > 0)
   {
-   cout<<"Applying swap step"<<endl;
+   cout<<"Applying atomic swap"<<endl;
    clus.type = "bimetallic";
+
     if(N_Simbolo_1>=N_Simbolo_2)
-    {
-       clus.swap(N_Simbolo_1);
-    }
-    else
     {
        clus.swap(N_Simbolo_2);
     }
+    else
+    {
+       clus.swap(N_Simbolo_1);
+    }
   }
+
+
+  // Applies kick for monometallic cases and non-swapping ones
+  // by calling "atomic.cpp"
   else
   {
-    //Doing the kick to the previous geometry
     if(kick==0)
     {
+      // random kick:
       cout<<"Kicking without potential"<<endl;
       clus.kick(step_width);
     }
+
+
     else
     {
+      // damped kick (jorge):
       cout<<"Kicking with lennard potential"<<endl;
       clus.kick_lennard(step_width); //Need to put range of -1 to 1 of the random value * step width
     }
   }
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  if(crystal==0){
+
+
+
+  // Update geometries if in the gas-phase:
+  if(crystal==0)
+   {
       geometry_file.clear();
-      geometry_file=file_name+"/geometry.in";
+      geometry_file="geometry.in";
       clus.print_fhi(geometry_file);
-  }
-  else{
+   }
+
+  //.. or periodic case:
+  else
+   {
       clus.move((x_max-x_min)/2.0+random_number(-dist,dist),(y_max-y_min)/2.0+random_number(-dist,dist),z_max-clus.z_min() );
       geometry_file.clear();
-      geometry_file=file_name+"/geometry.tmp";
+      geometry_file="geometry.tmp";
       clus.print_fhi(geometry_file);
-      command="cd "+file_name+" ; cat crystal.in > geometry.in ; cat geometry.tmp >> geometry.in ; rm geometry.tmp ";
+      command="cat crystal.in > geometry.in ; cat geometry.tmp >> geometry.in ; rm geometry.tmp ";
       system(command.c_str());
       command.clear();
-  }
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  command="cd "+file_name+" ; ./run.sh";
+   }
+
+
+  ////////////////////////////////////////////
+  // RUNNING FHI-AIMS SECOND CONFIGURATION....:
+  command="./run.sh";
   system(command.c_str());
   command.clear();
-  command="cd "+file_name+" ; grep 'Have a nice day' output.out | wc -l";
+
+  // Checking that geometry has converged:
+  command="grep 'Have a nice day' output.out | wc -l";
   contenido=int_pipe(command);
   command.clear();
 
+  //***********************************************************************************
+  // If structure DID NOT converged, then:
   while (contenido!=1)
   {
   cout<<" --> SCF failed.  "<<endl;
-// sera necesario borrar geometry.in???
+
+
   if(fail_counter<failed_max)
   {
      cout<<" Kicking ... again"<<endl;
      fail_counter++;
      geometry_file.clear();
-     geometry_file=file_name+"/coordinates"+i_str+".xyz";
+     geometry_file="coordinates"+i_str+".xyz";
      c_aux.read_xyz(geometry_file);
      geometry_file.clear();
      geometry_file="aux.fhi";
      c_aux.print_fhi(geometry_file);
-     if(N_Simbolo_2>0) //es bimetalico
+
+     // for bimetallics:
+     if(N_Simbolo_2>0)
      {
        clus_1=extract(geometry_file,Simbolo_1);
        clus_2=extract(geometry_file,Simbolo_2);
        clus  =clus_1+clus_2;
      }
-     else //es monometalico
+
+     // for monometallics:
+     else
      {
        clus=extract(geometry_file,Simbolo_1);
      }
+
+
      system("rm aux.fhi");
+
+
      // Applies swap or kick
      if(resto==0 && N_Simbolo_2 > 0)
      {
@@ -274,21 +366,27 @@ while(i+m <= iteraciones)
           clus.swap(N_Simbolo_2);
        }
      }
+
+
      else
      {
-       //aplica move;
        if(kick==0)
-       {
+       { // increase kick with damp to explore PES
          cout<<"Kicking without potential"<<endl;
          clus.kick(step_width+(fail_counter*damp));
        }
+
+
        else
-       {
+       { // increase kick with damp to explore PES
          cout<<"Kicking with lennard potential"<<endl;
          clus.kick_lennard(step_width+(fail_counter*damp));
        }
      }
   }
+
+
+
   else
   {
      cout<<" Starting again from randomly generated structure!"<<endl;
@@ -315,50 +413,77 @@ while(i+m <= iteraciones)
          }
      }
   }
+
+
   if(crystal==0){
       geometry_file.clear();
-      geometry_file=file_name+"/geometry.in";
+      geometry_file="geometry.in";
       clus.print_fhi(geometry_file);
   }
+
   else{
       clus.move((x_max-x_min)/2.0+random_number(-dist,dist),(y_max-y_min)/2.0+random_number(-dist,dist),z_max-clus.z_min() );
       geometry_file.clear();
-      geometry_file=file_name+"/geometry.tmp";
+      geometry_file="geometry.tmp";
       clus.print_fhi(geometry_file);
-      command="cd "+file_name+" ; cat crystal.in > geometry.in ; cat geometry.tmp >> geometry.in ; rm geometry.tmp ";
+      command="cat crystal.in > geometry.in ; cat geometry.tmp >> geometry.in ; rm geometry.tmp ";
       system(command.c_str());
       command.clear();
   }
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  command="cd "+file_name+" ;  ./run.sh";
+
+
+  // THIRD RUN FOR NON-CONVERGED STRUCTURES
+
+  command="./run.sh";
   system(command.c_str());
   command.clear();
-  command="cd "+file_name+" ; grep 'Have a nice day' output.out | wc -l";
+
+  // IF CONVERGED....
+  command="grep 'Have a nice day' output.out | wc -l";
   contenido=int_pipe(command.c_str());
   command.clear();
   }
+
+//***********************************************************************************************************************************
+
+
+
+
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 //                                         SAVE ENERGIES                                          //
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
 EnergiaAnterior=Energy;
-command="cd "+file_name+" ; grep \" | Total energy of the DFT \" output.out | awk '{print $12}' ";
+
+command="grep \" | Total energy of the DFT \" output.out | awk '{print $12}' ";
 Energy=float_pipe(command.c_str());
-E_str=string_pipe(command); //Modified
+E_str=string_pipe(command);
 command.clear();
-command=file_name+"/geometry.in.next_step";
+
+/////Perhaps this part can be inside Metropolis Monte-Carlo (in step of mv outi mv out)
+command="geometry.in.next_step";
 clus.read_fhi(command);
 command.clear();
-command=file_name+"/coordinates"+i_str+".xyz";
+
+
+command="coordinates"+i_str+".xyz";
 tag.clear();
 tag=" Iteration "+i_str+" -----> Energy = "+E_str+" eV ";
 clus.print_xyz(command,tag);
 command.clear();
-command="mv "+file_name+"/output.out "+file_name+"/output"+i_str+".out";
+
+
+command="mv output.out output"+i_str+".out";
 system(command.c_str());
 command.clear();
-command="mv "+file_name+"/geometry.in "+file_name+"/geometry"+i_str+".in";
+
+
+command="mv geometry.in geometry"+i_str+".in";
 system(command.c_str());
 command.clear();
+
+
+
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 //                                     Metropolis Monte-Carlo                                     //
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
@@ -367,29 +492,33 @@ if (pow(2.71,(EnergiaAnterior-Energy)/k_BT) > random_number(0,1))
 {
   cout<<"--> Basin Hopping MC criteria: Energy accepted! "<<endl;
   cout<<"--> Finished iteration "<<i<<endl;
-  command="echo "+i_str+" '---->'  "+E_str+" >> "+file_name+"/energies.txt";
+  command="echo "+i_str+" '---->'  "+E_str+" >> energies.txt";
   system(command.c_str());
   command.clear();
-  command="tail -"+i_str+" "+file_name+"/energies.txt |  sort -nk3 > "+file_name+"/sorted.txt";
+  command="tail -"+i_str+" energies.txt |  sort -nk3 > sorted.txt";
   system(command.c_str());
   command.clear();
   i++;
   fail_counter=0;
 }
+
+
 else
 {
-  m=1;
   cout<<"--> Basin Hopping MC criteria: Energy rejected!"<<endl;
+  m++;
   string m_str = to_string(m);
-  command="mv "+file_name+"/output"+i_str+".out "+file_name+"/rejected/outputrejected"+m_str+".out";
+  command="mv output"+i_str+".out ./rejected/outputrejected"+m_str+".out";
   system(command.c_str());
   command.clear();
-  command="mv "+file_name+"/geometry"+i_str+".in "+file_name+"/rejected/geometryrejected"+m_str+".in";
+  command="mv geometry"+i_str+".in ./rejected/geometryrejected"+m_str+".in";
   system(command.c_str());
   command.clear();
   m_str.clear();
-  m++;
+  fail_counter=0;
 }
-}
+
+
+} // END OF BH-LOOP
 return 0;
 }
